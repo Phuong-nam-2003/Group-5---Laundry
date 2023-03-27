90fe3e370b6816d8a9cc74b993d4f50132227dbb
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-03-27 14:21:16 +0700

    Huy 27/03/2023

---
+++ WebArtsShop/WebArtsShop/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebArtsShop.Models;
+
+namespace WebArtsShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ArtsShopContext _context;
+        public ProductController(ArtsShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId ==id);
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
